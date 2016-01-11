4a96edd3e9153a434bf5054a86147f5a052bffae
Hospitant Entwicklung <thomas@strothjohann.net>
2016-01-11 23:55:26 +0100

    Removed blog description, added margin-top to jdj.

--- header.php
+++ header.php
@@ -11,44 +11,41 @@
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo('charset');?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
 
-<?php wp_head(); ?>
+<?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?> >
+<body <?php body_class();?> >
 <div id="page" class="container">
 <div class="ribbon"><a href="#">Das ist eine beta-Version</a></div>
 	<header role="banner">
 	<div class="row">
     <div class="col-xs-12 col-md-6 col-md-offset-2 col-lg-4 col-lg-offset-3 m-t"><img class="img-fluid" src="<?php echo get_template_directory_uri() . '/images/mm-logo.png'; ?>"></div>
     <div class="col-xs-12">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+			<?php if (is_front_page() && is_home()): ?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></h1>
+			<?php endif;?>
 		</div>
 	</div>
 
-	  <?php wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'primary',
-                'depth'             => 0,
-                'container'         => 'nav',
-                'container_class'   => 'navbar navbar-light bg-faded',
-        				'container_id'      => 'navbar-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
-            );
-		?>
+	<?php wp_nav_menu(array(
+	'menu' => 'primary',
+	'theme_location' => 'primary',
+	'depth' => 0,
+	'container' => 'nav',
+	'container_class' => 'navbar navbar-light bg-faded',
+	'container_id' => 'navbar-1',
+	'menu_class' => 'nav navbar-nav',
+	'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+	'walker' => new wp_bootstrap_navwalker())
+);
+?>
 
 	</header>
