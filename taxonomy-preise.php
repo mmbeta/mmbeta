de0c97c15ce882de912ab0e0f7c58ba1e4499ae4
Hospitant Entwicklung <thomas@strothjohann.net>
2016-01-16 19:17:27 +0100

    Added template for othe terms than journalisten-des-jahres

--- taxonomy-preise.php
+++ taxonomy-preise.php
@@ -15,49 +15,52 @@ get_header();?>
     <div class="lead"><?php the_archive_description();?></div>
   </header><!-- .page-header -->
 
-<?php 
+<?php if(is_tax( 'preise', 'journalisten-des-jahres' )){
 
 
-function end_prev_letter() {
-   echo "<!-- End of letter-group -->\n";
-   echo "<div class='clear'></div>\n";
-}
-function start_new_letter($letter) {
-   echo "<div class='alert alert-success'>\n";
-   echo "\t<strong>$letter</strong>\n";
-   echo "</div>";
-}
-
-
-$args = array (
-  'posts_per_page' => -1,
-  'post_type' => 'preistraeger',
-  'meta_key' => 'nachname',
-  'orderby' => 'meta_value',
-  'order' => 'ASC',
-);
-query_posts($args);
-if ( have_posts() ) {
-  $in_this_row = 0;
-  while ( have_posts() ) {
-     the_post();
-     $first_letter = strtoupper(substr(get_field('nachname'),0,1));
-     if ($first_letter != $curr_letter) {
-        if (++$post_count > 1) {
-           end_prev_letter();
-        }
-        start_new_letter($first_letter);
-        $curr_letter = $first_letter;
-     }
-    get_template_part('teasers/teaser', get_post_type());
+  function end_prev_letter() {
+     echo "<!-- End of letter-group -->\n";
+     echo "<div class='clear'></div>\n";
   }
-  end_prev_letter();
-  ?>
-<?php } else {
-  echo "<h2>Sorry, no posts were found!</h2>";
-}
-?>
+  function start_new_letter($letter) {
+     echo "<div class='alert alert-success'>\n";
+     echo "\t<strong>$letter</strong>\n";
+     echo "</div>";
+  }
 
+
+  $args = array (
+    'posts_per_page' => -1,
+    'post_type' => 'preistraeger',
+    'meta_key' => 'nachname',
+    'orderby' => 'meta_value',
+    'order' => 'ASC',
+  );
+  query_posts($args);
+  if ( have_posts() ) {
+    $in_this_row = 0;
+    while ( have_posts() ) {
+       the_post();
+       $first_letter = strtoupper(substr(get_field('nachname'),0,1));
+       if ($first_letter != $curr_letter) {
+          if (++$post_count > 1) {
+             end_prev_letter();
+          }
+          start_new_letter($first_letter);
+          $curr_letter = $first_letter;
+       }
+      get_template_part('teasers/teaser', get_post_type());
+    }
+    end_prev_letter();
+    ?>
+  <?php } else {
+    echo "<h2>Sorry, no posts were found!</h2>";
+  }
+//end of jdj-archive
+}else{
+  get_template_part('template-parts/content', 'preis-uebersicht');
+
+}  ?>
 
 </main><!-- #main -->
 <?php get_footer();?>
---
+++ template-parts/content-preis-uebersicht.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Preistraeger Übersicht.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package medium_magazin_beta
+ */
+
+$kategorie = mmbeta_die_preiskategorie_object();
+$args = array(
+    'post_type' => 'preistraeger',
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'preise',
+        'field'    => 'slug',
+        'terms'    => $kategorie->slug,
+      ),
+    ),
+    'meta_key' => 'platz',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+  );
+  $query = new WP_Query( $args );
+
+
+if ($query->have_posts()): ?>
+  <div class="card-columns">
+      <?php while ($query->have_posts()): $query->the_post();?>
+        
+          <article class="card">
+            <div class="card-block">
+              <h4 class="card-title"><?php the_title(); ?></h4>
+              <p class="card-text"><?php the_field( "position" ); ?></p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><strong>Platz:</strong> <?php the_field('platz'); ?></li>
+              <li class="list-group-item"><strong>Kategorie:</strong> <?php mmbeta_die_preiskategorie(); ?></li>
+            </ul>
+            <div class="card-block">
+              <a href="<?php the_permalink(); ?>" class="btn btn-secondary">Begründung</a>  
+            </div>  
+          </article>
+      
+      <?php endwhile;?>
+  </div>
+<?php endif;?>
