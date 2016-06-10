9b2475f0485703a39d71d54eab4eedcf5ee5933f
TStrothjohann <thomas@strothjohann.net>
2016-06-10 18:34:08 +0200

    typo

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -403,7 +403,7 @@ add_shortcode('more', 'show_more_function');
 
 function iframe_shortcode($attr, $url) {
 
- $style = "<style type="text/css">.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>";
+ $style = "<style type='text/css'>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>";
 
  $output = $style;
  $output .= "<div class='embed-container'><iframe src='";
