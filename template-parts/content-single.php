863c769b013edd7a1470e76ae4acae4cbb6a230f
TStrothjohann <thomas@strothjohann.net>
2016-10-31 22:35:32 +0100

    removed char limit on hp teaser cf and overflowting footer on article pages

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,15 +9,18 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="col-xs-12 col-lg-6 col-lg-offset-3" >
-	<header class="entry-header">
-		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
-	</header><!-- .entry-header -->
-	<div>
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-	<small class="entry-meta">
-		<?php mmbeta_posted_on(); ?>
-	</small><!-- .entry-meta -->
 
-</article><!-- #post-## -->
+<div class="row">
+  <article id="post-<?php the_ID(); ?>" class="col-xs-12 col-lg-6 col-lg-offset-3" >
+  	<header class="entry-header">
+  		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+  	</header><!-- .entry-header -->
+  	<div>
+  		<?php the_content(); ?>
+  	</div><!-- .entry-content -->
+  	<small class="entry-meta">
+  		<?php mmbeta_posted_on(); ?>
+  	</small><!-- .entry-meta -->
+
+  </article><!-- #post-## -->
+</div>
--- template-parts/content.php
+++ template-parts/content.php
@@ -9,16 +9,18 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="col-lg-6 col-lg-offset-3" >
-	<header class="entry-header">
-		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
-	</header><!-- .entry-header -->
+<div class="row">
+  <article id="post-<?php the_ID(); ?>" class="col-lg-6 col-lg-offset-3" >
+  	<header class="entry-header">
+  		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+  	</header><!-- .entry-header -->
 
-	<div>
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-	<small class="entry-meta col-xs-12 col-lg-8 col-lg-offset-2">
-		<?php mmbeta_posted_on(); ?>
-	</small><!-- .entry-meta -->
+  	<div>
+  		<?php the_content(); ?>
+  	</div><!-- .entry-content -->
+  	<small class="entry-meta col-xs-12 col-lg-8 col-lg-offset-2">
+  		<?php mmbeta_posted_on(); ?>
+  	</small><!-- .entry-meta -->
 
-</article><!-- #post-## -->
+  </article><!-- #post-## -->
+</div>
