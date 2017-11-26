36bc16347f2c676cce622cd95c9ede4c022e22dd
TStrothjohann <thomas@strothjohann.net>
2017-11-26 15:15:10 +0100

    basic styling aufmacher

--- hp/hp-aufmacher.php
+++ hp/hp-aufmacher.php
@@ -9,23 +9,26 @@
 
 ?>
 
-<div class="row" style="background-color: <?php echo( mmbeta_color($post->background_color) ); ?>">
+<div class="row flex-container" style="background-color: <?php echo( mmbeta_color($post->background_color) ); ?>">
 
   <?php
-  // echo mmbeta_color($post->contrast_color);
+  $contrast_color = mmbeta_color($post->contrast_color);
+  $attributes = $arrayName = array('class' => 'figure-img aufmacher');
   //Bild
-  echo wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'cover' );
-  
+  echo '<div class="flex-item aufmacher-img-container">' . wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'cover', false, $attributes  ) . '</div>';
+  ?>
+  <div class="flex-item aufmacher-text-container">
+  <?php
   // Themenliste
   if( have_rows('themen') ):
-      echo "<ul>";
+      echo "<ul class='list-unstyled'>";
     // loop through the rows of data
       while ( have_rows('themen') ) : the_row();
   ?>
 
-          <li>
-            <?php the_sub_field('topic-title'); ?>
-            <?php the_sub_field('topic-teaser'); ?>
+          <li class="aufmacher-list-item">
+            <h5 class="topic-title"><?php the_sub_field('topic-title'); ?></h5>
+            <p class="topic-teaser"><?php the_sub_field('topic-teaser'); ?></p>
           </li>
 
   <?php
@@ -38,4 +41,6 @@
   endif;
 
   ?>
+    <button type="button" class="btn" style="background-color: <?php echo $contrast_color; ?>; border-color: <?php echo $contrast_color; ?>;">Heft kaufen</button>
+  </div>
 </div>
