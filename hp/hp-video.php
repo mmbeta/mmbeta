e2b1f5afe87dd9548e85ceec46cbc05620c96efd
TStrothjohann <thomas@strothjohann.net>
2017-11-28 22:20:00 +0100

    Video works.

--- hp/hp-video.php
+++ hp/hp-video.php
@@ -10,24 +10,27 @@
   $embed = get_sub_field('embed_element');
 ?>
 
+<div class="row m-t">
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 col-xl-8 col-xl-offset-2">
+    <div class="col-xs-12">
+      <div class="card teaser teaser1-body">
+        <div class="col-xs-12 col-md-6">
+          <?php if ( $embed ):
+            
+            echo '<div class="embed-container">';
+              echo $embed;   
+            echo '</div>';
 
-<div class="col-xs-12">
-  <div class="card teaser teaser1-body">
-    <div class="col-xs-12 col-md-6 col-lg-4">
-      <?php if ( $embed ):
-        
-        echo '<div class="embed-container">';
-          echo $embed;   
-        echo '</div>';
-
-      endif; ?> 
-    </div>
-    <div class="col-xs-12 col-md-6 col-lg-8">
-      <h3 class="card-title teaser1-header"><?php the_sub_field('embed-teaser-titel'); ?></h3>
-      <div class="teaser1-text">
-        <?php 
-          the_sub_field('embed-teaser-text');
-        ?>
+          endif; ?> 
+        </div>
+        <div class="col-xs-12 col-md-6">
+          <h3 class="card-title teaser1-header"><?php echo get_sub_field('embed-teaser-titel'); ?></h3>
+          <div class="teaser1-text">
+            <?php 
+              echo get_sub_field('embed-teaser-text');
+            ?>
+          </div>
+        </div>
       </div>
     </div>
   </div>
