f4d567aafc895c8aa0ee9406b8af530ba2bb05a7
Thomas Strothjohann <thomas@strothjohann.net>
2018-05-29 22:47:28 +0200

    Mobile styling is ok

--- searchform.php
+++ searchform.php
@@ -10,13 +10,9 @@
 
 ?>
 
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
-    <fieldset>
-		<div class="input-group">
-			<input type="text" name="s" id="search" placeholder="<?php _e("Search","mmbeta"); ?>" value="<?php the_search_query(); ?>" class="form-control" />
-			<span class="input-group-btn">
-				<button type="submit" class="btn btn-default"><?php _e("Search","mmbeta"); ?></button>
-			</span>
-		</div>
-    </fieldset>
+<form action="<?php echo home_url( '/' ); ?>" method="get" class="input-group">
+  <input type="text" value="<?php the_search_query(); ?>" name="s" id="search" class="form-control" placeholder="Suche" aria-label="Suche">
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="submit">Suchen</button>
+  </div>
 </form>
