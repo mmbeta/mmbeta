eb35ad11b2d896c6b70bfad397b1c10afff2e7fb
Thomas Strothjohann <thomas@strothjohann.net>
2018-05-29 22:54:45 +0200

    search icon from material design

--- searchform.php
+++ searchform.php
@@ -13,6 +13,8 @@
 <form action="<?php echo home_url( '/' ); ?>" method="get" class="input-group">
   <input type="text" value="<?php the_search_query(); ?>" name="s" id="search" class="form-control" placeholder="Suche" aria-label="Suche">
   <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="submit">Suchen</button>
+    <button class="btn btn-outline-secondary" type="submit">
+    	<img src="<?php print get_template_directory_uri() . '/images/baseline-search-24px.svg'  ?>" width="24" height="30" alt="Suchen">
+    </button>
   </div>
 </form>
