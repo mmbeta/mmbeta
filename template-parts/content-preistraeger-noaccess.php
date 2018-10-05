ba2b26e6dda15f625f82196a3e69dd0cd3990708
Thomas Strothjohann <thomas@strothjohann.net>
2018-10-05 23:20:16 +0200

    added redeem voucher form to noaccess pages

--- template-parts/content-preistraeger-noaccess.php
+++ template-parts/content-preistraeger-noaccess.php
@@ -22,6 +22,20 @@
       ?>
     </p>
     <?php global $post; ?>
-    <a href="<?php echo mmbeta_get_laterpay_purchase_link( $post->tax_page ); ?>" class="btn btn-outline-secondary">Jetzt kaufen</a> 
+    <a href="<?php echo mmbeta_get_laterpay_purchase_link( $post->tax_page ); ?>" class="btn btn-lg btn-outline-secondary">Jetzt kaufen</a> 
+
+    <!-- Test redeem voucher -->
+
+      <div id="lp_js_giftCardWrapper" class="lp_js_giftCodeWrapper lp_js_dataDeferExecution lp_redeem-code__wrapper lp_clearfix">
+        <input type="text" name="gift_code" class="lp_js_giftCardCodeInput lp_redeem-code__value" maxlength="6">
+        <p class="lp_redeem-code__input-hint"><?php esc_html_e( 'Gutscheincode', 'laterpay' ); ?></p>
+        <a href="#" class="lp_js_giftCardRedeemButton lp_redeem-code__button lp_button"><?php esc_html_e( 'Redeem', 'laterpay' ); ?></a>
+      </div>
+
+      <a href="#" id="fakebtn" class="lp_js_doPurchase" style="display:none;" data-laterpay="" data-preview-as-visitor="<?php echo esc_attr( $laterpay['preview_post_as_visitor'] ); ?>"></a>
+
+    <!-- End Test -->
+
+
   </div>
 </div>
